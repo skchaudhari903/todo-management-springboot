a166cdc93c5406f0a24d43c171f21d7be875b2cc
Shubham Chaudhary <skchaudhari903@gmail.com>
2021-12-31 23:36:35 +0530

    Initial commit

---
+++ src/test/java/com/skc/springboot/todomanagement/TodoManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skc.springboot.todomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
